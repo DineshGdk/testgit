edc33a3f27dee5a8b4b79725182a6ef17ec8b300
DineshGdk <30556817+DineshGdk@users.noreply.github.com>
2018-03-26 12:37:00 +0530

    changes in the code

--- file_java.java
+++ file_java.java
@@ -1,6 +1,8 @@
 import java.io.*;
 class file_java{	
 	public static void main(String args[]){
-		System.out.println("hii from java at 3:53");
+		int a=1;
+		for(int i=0;i<8;i++)
+		System.out.println("hii from java at",+(a++)");
 	}
 }	
