0d0167bfc44b64ffe3d2295bf403436cd811d029
DineshGdk <30556817+DineshGdk@users.noreply.github.com>
2018-04-02 12:40:41 +0530

    Update file_java.java
    comment some lines

--- file_java.java
+++ file_java.java
@@ -1,7 +1,7 @@
 import java.io.*;
 class file_java{	
 	public static void main(String args[]){
-		int a=1;
+	//	int a=1;
 		for(int i=0;i<8;i++)
 		System.out.println("hii from java at",+(a++)");
 	}
