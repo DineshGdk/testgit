3c84f0cc137929acaffd804ad21112408e007ae2
DineshGdk <30556817+DineshGdk@users.noreply.github.com>
2018-04-03 17:47:36 +0530

    change made in githutb

--- file_java.java
+++ file_java.java
@@ -3,6 +3,6 @@ class file_java{
 	public static void main(String args[]){
 		int a=1;
 		for(int i=0;i<8;i++)
-		System.out.println("hii from java at",+(a++)");
+		System.out.println("hii from java at",+(a++));
 	}
 }	
